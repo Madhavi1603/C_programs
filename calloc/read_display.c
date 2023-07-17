01bd4d791af446a89fb638652c880e658570ccf7
Ravula Madhavi <ravula.madhavi03@gmail.com>
2023-07-17 15:14:44 +0530

    programs on calloc

---
+++ calloc/read_display.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int size,i,*array;
+printf("size of array\n");
+scanf("%d",&size);
+array=(int*)calloc(size,sizeof(int));
+printf("enter the values of array\n");
+for(i=0;i<size;i++)
+scanf("%d",&array[i]);
+printf("the values in the array are :\n");
+for(i=0;i<size;i++)
+{
+printf("%d\n",array[i]);
+}
+}
